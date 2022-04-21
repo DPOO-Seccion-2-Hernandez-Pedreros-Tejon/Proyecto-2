7dfd8ed0bd3785cea3436ac943b0905080681919
Wichy76 <l.tejon@uniandes.edu.co>
2022-04-21 06:52:57 -0500

    c

--- Proyecto 2 Entrega 2/src/view/Inicio.java
+++ Proyecto 2 Entrega 2/src/view/Inicio.java
@@ -7,6 +7,9 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Window;
+
 import javax.swing.SwingConstants;
 import java.awt.Color;
 import net.miginfocom.swing.MigLayout;
@@ -14,14 +17,16 @@ import javax.swing.JTextField;
 import javax.swing.border.MatteBorder;
 import javax.swing.border.LineBorder;
 import java.awt.FlowLayout;
+import java.awt.Component;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.awt.event.ActionEvent;
 
 public class Inicio {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -57,97 +62,314 @@ public class Inicio {
 		frame.setLocationRelativeTo(null);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
-		panel_1.setLayout(new BorderLayout(0, 0));
+		JPanel principal = new JPanel();
+		frame.getContentPane().add(principal, BorderLayout.CENTER);
+		principal.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel_2.setBackground(new Color(102, 153, 204));
-		panel_1.add(panel_2, BorderLayout.NORTH);
+		principal.add(panel_2, BorderLayout.NORTH);
 		panel_2.setLayout(new MigLayout("", "[][100px,grow,fill][leading][100px:n]", "[]"));
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
+		JLabel lblNewLabel_1 = new JLabel("");
 		panel_2.add(lblNewLabel_1, "cell 0 0");
 		
 		JLabel lblNewLabel_2 = new JLabel("Fecha:");
 		panel_2.add(lblNewLabel_2, "cell 2 0,alignx trailing,growy");
 		
-		textField = new JTextField();
-		textField.setEditable(false);
-		panel_2.add(textField, "cell 3 0,growx");
-		textField.setColumns(10);
-		
-		JPanel panel_3 = new JPanel();
-		panel_1.add(panel_3, BorderLayout.CENTER);
-		panel_3.setLayout(new MigLayout("", "[grow][][grow]", "[grow]"));
-		
-		JPanel panel = new JPanel();
-		panel_3.add(panel, "cell 0 0,grow");
-		panel.setLayout(new MigLayout("", "[grow]", "[grow,center][grow,center][grow,center][grow,center]"));
-		
-		JLabel lblNewLabel_4 = new JLabel("Iniciar Sesi\u00F3n");
-		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(lblNewLabel_4, "cell 0 0,growx");
-		
-		JPanel panel_5 = new JPanel();
-		panel.add(panel_5, "cell 0 1,grow");
-		panel_5.setLayout(null);
-		
-		JLabel lblNewLabel_4_1 = new JLabel("Ingrese su nombre de usuario:");
-		lblNewLabel_4_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_4_1.setBounds(176, 41, 151, 14);
-		panel_5.add(lblNewLabel_4_1);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(201, 78, 96, 20);
-		panel_5.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JPanel panel_6 = new JPanel();
-		panel.add(panel_6, "cell 0 2,grow");
-		
-		JPanel panel_7 = new JPanel();
-		panel.add(panel_7, "cell 0 3,grow");
-		
-		JLabel lblNewLabel_3 = new JLabel("");
-		panel_3.add(lblNewLabel_3, "cell 1 0");
-		
-		JPanel panel_4 = new JPanel();
-		panel_3.add(panel_4, "cell 2 0,grow");
-		panel_4.setLayout(null);
-		
-		JLabel lblNewLabel_4_2 = new JLabel("Iniciar Sesi\u00F3n");
-		lblNewLabel_4_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_4_2.setBounds(-25, 62, 520, 14);
-		panel_4.add(lblNewLabel_4_2);
-		
-		JLabel lblNewLabel_4_1_1 = new JLabel("Ingrese su nombre de usuario:");
-		lblNewLabel_4_1_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_4_1_1.setBounds(160, 174, 151, 14);
-		panel_4.add(lblNewLabel_4_1_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(183, 216, 96, 20);
-		panel_4.add(textField_2);
-		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(183, 333, 96, 20);
-		panel_4.add(textField_3);
-		
-		JLabel lblNewLabel_4_1_1_1 = new JLabel("Ingrese su nombre de usuario:");
-		lblNewLabel_4_1_1_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_4_1_1_1.setBounds(160, 294, 151, 14);
-		panel_4.add(lblNewLabel_4_1_1_1);
-		
 		JLabel lblNewLabel = new JLabel("Project Manager");
 		lblNewLabel.setLabelFor(panel_2);
+		
+		JLabel lblNewLabel_5 = new JLabel(darFechaddMM());
+		panel_2.add(lblNewLabel_5, "cell 3 0");
 		lblNewLabel.setBackground(new Color(51, 102, 153));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 57));
 		lblNewLabel.setOpaque(true);
 		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		
+		crearPanelInicioSesion(principal);
+			
 	}
+	
+	
+	public void crearPanelInicioSesion(JPanel panel_1)
+
+	{
+		JPanel panelInicioSesion = new JPanel();
+		panelInicioSesion.setForeground(new Color(255, 255, 255));
+		panelInicioSesion.setBackground(Color.DARK_GRAY);
+		panel_1.add(panelInicioSesion, BorderLayout.CENTER);
+		panelInicioSesion.setLayout(new MigLayout("", "[grow][][grow]", "[grow]"));
+		
+		JPanel panel = new JPanel();
+		panel.setForeground(new Color(255, 255, 255));
+		panel.setBackground(Color.DARK_GRAY);
+		panelInicioSesion.add(panel, "cell 0 0,grow");
+		panel.setLayout(new MigLayout("", "[grow]", "[grow,center][grow,center][grow,center][grow,center]"));
+		
+		JLabel lblNewLabel_4 = new JLabel("Inicia t\u00FA sesi\u00F3n");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel_4, "cell 0 0,growx");
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setForeground(new Color(255, 255, 255));
+		panel_5.setBackground(Color.DARK_GRAY);
+		panel.add(panel_5, "cell 0 1,grow");
+		panel_5.setLayout(null);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("Ingrese su nombre de usuario:");
+		lblNewLabel_4_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4_1.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4_1.setBounds(0, 39, 451, 13);
+		panel_5.add(lblNewLabel_4_1);
+		
+		JTextField textField_1 = new JTextField();
+		textField_1.setForeground(new Color(255, 255, 255));
+		textField_1.setBackground(Color.DARK_GRAY);
+		textField_1.setBounds(155, 77, 142, 20);
+		panel_5.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setForeground(new Color(255, 255, 255));
+		panel_6.setBackground(Color.DARK_GRAY);
+		panel.add(panel_6, "cell 0 2,grow");
+		panel_6.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Continuar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				panelInicioSesion.setVisible(false);
+				panel_1.remove(panelInicioSesion);
+				crearPanelNuevoProyecto(panel_1);
+			}
+		});
+		btnNewButton.setBackground(new Color(102, 153, 255));
+		btnNewButton.setBounds(160, 52, 125, 21);
+		panel_6.add(btnNewButton);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setForeground(new Color(255, 255, 255));
+		panel_7.setBackground(Color.DARK_GRAY);
+		panel.add(panel_7, "cell 0 3,grow");
+		
+		JLabel lblNewLabel_3 = new JLabel("\u00D3");
+		lblNewLabel_3.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblNewLabel_3.setOpaque(true);
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setBackground(Color.DARK_GRAY);
+		panelInicioSesion.add(lblNewLabel_3, "flowx,cell 1 0");
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setForeground(new Color(255, 255, 255));
+		panel_4.setBackground(Color.DARK_GRAY);
+		panelInicioSesion.add(panel_4, "cell 2 0,grow");
+		panel_4.setLayout(new MigLayout("", "[grow]", "[grow,center][grow,center][grow,center][grow,center]"));
+		
+		JLabel lblNewLabel_4_2 = new JLabel("Crea un usuario nuevo");
+		lblNewLabel_4_2.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblNewLabel_4_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4_2.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4_2.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_4.add(lblNewLabel_4_2, "cell 0 0,alignx center");
+		
+		JPanel panel_5_1 = new JPanel();
+		panel_5_1.setForeground(new Color(255, 255, 255));
+		panel_5_1.setBackground(Color.DARK_GRAY);
+		panel_5_1.setLayout(null);
+		panel_4.add(panel_5_1, "cell 0 1,grow");
+		
+		JTextField textField_2 = new JTextField();
+		textField_2.setForeground(new Color(255, 255, 255));
+		textField_2.setBackground(Color.DARK_GRAY);
+		textField_2.setColumns(10);
+		textField_2.setBounds(177, 77, 142, 20);
+		panel_5_1.add(textField_2);
+		
+		JLabel lblNewLabel_4_1_1 = new JLabel("Ingrese su nombre de usuario:");
+		lblNewLabel_4_1_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4_1_1.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4_1_1.setBounds(0, 37, 480, 13);
+		panel_5_1.add(lblNewLabel_4_1_1);
+		
+		JPanel panel_5_2 = new JPanel();
+		panel_5_2.setForeground(new Color(255, 255, 255));
+		panel_5_2.setBackground(Color.DARK_GRAY);
+		panel_5_2.setLayout(null);
+		panel_4.add(panel_5_2, "cell 0 2,grow");
+		
+		JTextField textField_3 = new JTextField();
+		textField_3.setForeground(new Color(255, 255, 255));
+		textField_3.setBackground(Color.DARK_GRAY);
+		textField_3.setColumns(10);
+		textField_3.setBounds(175, 77, 142, 20);
+		panel_5_2.add(textField_3);
+		
+		JLabel lblNewLabel_4_1_2 = new JLabel("Ingrese su nombre de usuario:");
+		lblNewLabel_4_1_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4_1_2.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4_1_2.setBounds(0, 36, 480, 13);
+		panel_5_2.add(lblNewLabel_4_1_2);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBackground(Color.DARK_GRAY);
+		panel_4.add(panel_8, "cell 0 3,grow");
+		panel_8.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("Continuar");
+		btnNewButton_1.setBackground(new Color(102, 153, 255));
+		btnNewButton_1.setBounds(184, 49, 125, 21);
+		panel_8.add(btnNewButton_1);
+	}
+
+	public void crearPanelNuevoProyecto(JPanel principal)
+	{
+		JPanel panelCrearProyecto = new JPanel();
+		panelCrearProyecto.setForeground(new Color(255, 255, 255));
+		panelCrearProyecto.setBackground(Color.DARK_GRAY);
+		principal.add(panelCrearProyecto, BorderLayout.CENTER);
+		panelCrearProyecto.setLayout(new MigLayout("", "[][][grow]", "[grow]"));
+		
+		JPanel panel = new JPanel();
+		panel.setForeground(new Color(255, 255, 255));
+		panel.setBackground(Color.DARK_GRAY);
+		panelCrearProyecto.add(panel, "cell 0 0,alignx left,growy");
+		panel.setLayout(new MigLayout("", "[grow]", "[grow,center][grow,center][grow,center][grow,center]"));
+		
+		JLabel lblNewLabel_4 = new JLabel("Inicia t\u00FA sesi\u00F3n");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel_4, "cell 0 0,growx");
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setForeground(new Color(255, 255, 255));
+		panel_5.setBackground(Color.DARK_GRAY);
+		panel.add(panel_5, "cell 0 1,grow");
+		panel_5.setLayout(null);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("Ingrese su nombre de usuario:");
+		lblNewLabel_4_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4_1.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4_1.setBounds(0, 39, 451, 13);
+		panel_5.add(lblNewLabel_4_1);
+		
+		JTextField textField_1 = new JTextField();
+		textField_1.setForeground(new Color(255, 255, 255));
+		textField_1.setBackground(Color.DARK_GRAY);
+		textField_1.setBounds(155, 77, 142, 20);
+		panel_5.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setForeground(new Color(255, 255, 255));
+		panel_6.setBackground(Color.DARK_GRAY);
+		panel.add(panel_6, "cell 0 2,grow");
+		panel_6.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Continuar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panelCrearProyecto.setVisible(false);
+			}
+		});
+		btnNewButton.setBackground(new Color(102, 153, 255));
+		btnNewButton.setBounds(160, 52, 125, 21);
+		panel_6.add(btnNewButton);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setForeground(new Color(255, 255, 255));
+		panel_7.setBackground(Color.DARK_GRAY);
+		panel.add(panel_7, "cell 0 3,grow");
+		
+		JLabel lblNewLabel_3 = new JLabel("\u00D3");
+		lblNewLabel_3.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblNewLabel_3.setOpaque(true);
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setBackground(Color.DARK_GRAY);
+		panelCrearProyecto.add(lblNewLabel_3, "flowx,cell 1 0");
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setForeground(new Color(255, 255, 255));
+		panel_4.setBackground(Color.DARK_GRAY);
+		panelCrearProyecto.add(panel_4, "cell 2 0,grow");
+		panel_4.setLayout(new MigLayout("", "[grow]", "[grow,center][grow,center][grow,center][grow,center]"));
+		
+		JLabel lblNewLabel_4_2 = new JLabel("Crea un usuario nuevo");
+		lblNewLabel_4_2.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblNewLabel_4_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4_2.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4_2.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_4.add(lblNewLabel_4_2, "cell 0 0,alignx center");
+		
+		JPanel panel_5_1 = new JPanel();
+		panel_5_1.setForeground(new Color(255, 255, 255));
+		panel_5_1.setBackground(Color.DARK_GRAY);
+		panel_5_1.setLayout(null);
+		panel_4.add(panel_5_1, "cell 0 1,grow");
+		
+		JTextField textField_2 = new JTextField();
+		textField_2.setForeground(new Color(255, 255, 255));
+		textField_2.setBackground(Color.DARK_GRAY);
+		textField_2.setColumns(10);
+		textField_2.setBounds(177, 77, 142, 20);
+		panel_5_1.add(textField_2);
+		
+		JLabel lblNewLabel_4_1_1 = new JLabel("Ingrese su nombre de usuario:");
+		lblNewLabel_4_1_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4_1_1.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4_1_1.setBounds(0, 37, 480, 13);
+		panel_5_1.add(lblNewLabel_4_1_1);
+		
+		JPanel panel_5_2 = new JPanel();
+		panel_5_2.setForeground(new Color(255, 255, 255));
+		panel_5_2.setBackground(Color.DARK_GRAY);
+		panel_5_2.setLayout(null);
+		panel_4.add(panel_5_2, "cell 0 2,grow");
+		
+		JTextField textField_3 = new JTextField();
+		textField_3.setForeground(new Color(255, 255, 255));
+		textField_3.setBackground(Color.DARK_GRAY);
+		textField_3.setColumns(10);
+		textField_3.setBounds(175, 77, 142, 20);
+		panel_5_2.add(textField_3);
+		
+		JLabel lblNewLabel_4_1_2 = new JLabel("Ingrese su nombre de usuario:");
+		lblNewLabel_4_1_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4_1_2.setBackground(Color.DARK_GRAY);
+		lblNewLabel_4_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4_1_2.setBounds(0, 36, 480, 13);
+		panel_5_2.add(lblNewLabel_4_1_2);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBackground(Color.DARK_GRAY);
+		panel_4.add(panel_8, "cell 0 3,grow");
+		panel_8.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("Continuar");
+		btnNewButton_1.setBackground(new Color(102, 153, 255));
+		btnNewButton_1.setBounds(184, 49, 125, 21);
+		panel_8.add(btnNewButton_1);
+		panelCrearProyecto.setVisible(true);
+	}
+
+	public static String darFechaddMM() {
+		LocalDateTime date = LocalDateTime.now();
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	    String strDate = date.format(myFormatObj);
+		return strDate;
+	}
+	
 }
